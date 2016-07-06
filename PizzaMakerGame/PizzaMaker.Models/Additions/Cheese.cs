c9927ebaf4b00b6d80b587a29398df77a4da804b
Gergana Yordanova <gergana.yordanova@gmail.com>
2016-07-06 21:39:59 +0300

    New Cheese class

--- PizzaMakerGame/PizzaMaker.Models/Additions/Cheese.cs
+++ PizzaMakerGame/PizzaMaker.Models/Additions/Cheese.cs
@@ -4,16 +4,22 @@
 
     public class Cheese : Addition
     {
-        private decimal price = 2m;
+        const decimal PRICE = 1m;
+        const int QUANTITY_PER_SERVING = 50; // quantity per serving
+        const int CALORIES = 120;
+        const bool VEGETARIAN = false;
 
         public Cheese(int quantity)
             : base(quantity)
         {
+            this.isVegetarian = VEGETARIAN;
+            this.calories = quantity * CALORIES;
+            this.quantity = quantity * QUANTITY_PER_SERVING;
         }
-
+        
         public override decimal Price
         {
-            get { return this.price; }
+            get { return PRICE; }
         }
     }
 }
