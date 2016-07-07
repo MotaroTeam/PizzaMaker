f49f0a271c3ce79abccb3e0c8a0eaf010ba1813f
Gergana Yordanova <gergana.yordanova@gmail.com>
2016-07-07 19:03:31 +0300

    Sauces enum

---
+++ PizzaMakerGame/PizzaMaker.Models/Enums/Sauces.cs
@@ -0,0 +1,13 @@
+﻿namespace PizzaMaker.Models.Enums
+{
+    public enum Sauces
+    {
+        Alfredo = 0,
+        Chily = 1,
+        GarlicSauce = 2,
+        Ketchup = 3,
+        Marinara = 4,
+        Pesto = 5,
+        Ranch = 6
+    }
+}
