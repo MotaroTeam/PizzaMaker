c1a3fa52269890b1974673311b1206cff1be5836
lllevski <mitkostoikov1988@gmail.com>
2016-07-08 22:13:35 +0300

    validation folder

---
+++ PizzaMakerGame/PizzaMaker.Models/Globals/Validation/ArgumentValidation.cs
@@ -0,0 +1,19 @@
+﻿namespace PizzaMaker.Models.Globals.Validation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ArgumentValidation
+    {
+        public static void NegativeValueValidation(int data, string target)
+        {
+            if (data < 0)
+            {
+                throw new ArgumentException(target + "cannot have a negative value!");
+            }
+        }
+    }
+}
