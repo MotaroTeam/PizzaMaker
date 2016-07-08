a338758bc666be490a8da496712631b99be36aeb
lllevski <mitkostoikov1988@gmail.com>
2016-07-08 22:56:12 +0300

    added custom exception

---
+++ PizzaMakerGame/PizzaMaker.Models/Globals/Validation/AdditionOverload.cs
@@ -0,0 +1,21 @@
+﻿namespace PizzaMaker.Models.Globals.Validation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class AdditionOverload : Exception
+    {
+        public AdditionOverload(string message)
+        : base(message)
+        {
+        }
+
+        public AdditionOverload(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
