a17d2cfb8f5164bad594c58c28005cfdc5b2f3de
Gergana Yordanova <gergana.yordanova@gmail.com>
2016-07-10 12:58:10 +0300

    Add files via upload

--- PizzaMakerGame/PizzaMaker.Models/Globals/GlobalConstants.cs
+++ PizzaMakerGame/PizzaMaker.Models/Globals/GlobalConstants.cs
@@ -9,7 +9,7 @@
     public class GlobalConstants
     {
         // All constant values are represented in grams/ml PER SERVING!
-        public const int VEGETABLES_QUANTITY = 30;   
+        public const int VEGETABLES_QUANTITY = 60;   
 
         public const int MEAT_QUANTITY = 40;
 
@@ -17,9 +17,9 @@
 
         public const int CHEESE_QUANTITY = 40;
 
-        public const int FRUIT_QUANTITY = 10;
+        public const int FRUIT_QUANTITY = 50;
 
-        public const int SAUCE_QUANTITY = 25;
+        public const int SAUCE_QUANTITY = 30;
 
         public const decimal SAUCE_PRICE = 0.02m; // Per grams.
 
