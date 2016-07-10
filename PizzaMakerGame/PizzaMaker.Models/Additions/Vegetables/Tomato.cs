25adc675445472144472ef7b669ba721a9cef1fc
Gergana Yordanova <gergana.yordanova@gmail.com>
2016-07-10 13:02:30 +0300

    Add files via upload

---
+++ PizzaMakerGame/PizzaMaker.Models/Additions/Vegetables/Tomato.cs
@@ -0,0 +1,33 @@
+﻿namespace PizzaMaker.Models.Additions.Vegetables
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using BaseClasses;
+    using Globals;
+
+    public class Tomato : Addition
+    {
+        public const decimal PRICE = 0.03m;
+        public const int CALORIES = 11;
+        public const bool VEGETARIAN = true;
+
+        public Tomato(int quantity)
+            : base(quantity)
+        {
+            this.IsVegetarian = VEGETARIAN;
+            this.Calories = quantity * CALORIES;
+            this.Quantity = quantity * GlobalConstants.VEGETABLES_QUANTITY;
+        }
+
+        public override decimal Price
+        {
+            get
+            {
+                return PRICE * this.Quantity;
+            }
+        }
+    }
+}
